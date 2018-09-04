61b52d6aad012fbc7e67603821965b01f591c439
jdm79 <james.malvern@gmail.com>
2018-09-04 17:12:58 +0100

    adds stormy weather error checks

--- src/Airport.js
+++ src/Airport.js
@@ -7,5 +7,12 @@ Airport.prototype.clearForLanding = function(plane) {
   this._hangar.push(plane)
 }
 Airport.prototype.clearForTakeoff = function(plane) {
+  if(this.isStormy()) {
+    throw new Error('cannot take off during storm')
+  }
   this._hangar = []
+}
+
+Airport.prototype.isStormy = function() {
+  return false
 }
